770e3fbb507e9ced0199cce5c6674aa259495178
NikitaKonkov <anisimovnkonkov1ck@gmail.com>
2025-08-19 11:37:49 +0300

    Работа над фронтендом. Добавление дизайна и начального функционала

--- frontend/templates/js/main.js
+++ frontend/templates/js/main.js
@@ -1,4 +1,24 @@
+fetch("http://localhost:8080/", { mode: 'cors' })
+.then(response => response.json())
+.then(data => pars(data));
 
+let container = document.getElementById('columns');
+
+function pars(data) {
+    for (let i = 0; i < Object.keys(data).length; i++) {
+        container.innerHTML = '<div class="column">' +
+            '<div class="image-container">' +
+                '<img src="'+ data[i]['url_on_img'] +'" alt="Приём ' + data[i]['id'] + '">' +
+                '<div class="image-caption">'+ data[i]['name'] + '</div>' +
+                '<div class="image-resume">'+ data[i]['resume'] + '</div>' +
+                '<button id="put_priem">Редактировать</button>' +
+                '<button id="delete_priem">Удалить</button>' +
+            '</div>';
+    }
+}
+
+
+/*
 const clickButton = document.querySelector('button');
 if (clickButton) {
     clickButton.addEventListener('click', showMessage)
@@ -9,3 +29,5 @@ function showMessage() {
    .then(response => response.json())
    .then(data => console.log(data))
 }
+
+*/
